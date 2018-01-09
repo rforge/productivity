506f0731520b4daf1c6cca6bf4bf4ab74f49c6bb
ydesjeux <ydesjeux@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-01-09 15:22:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/productivity@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/productivity/R/zzz.R
+++ pkg/productivity/R/zzz.R
@@ -5,6 +5,5 @@ globalVariables(c("ano", "dmu"))
         "  Dakpo K.H., Desjeux Y. and Latruffe L. (2018). productivity: Indices of Productivity and Profitability Using Data Envelopment Analysis (DEA). R package version 1.1.0.\n\n", 
         "See also: citation(\"productivity\")\n\n", 
         "* For any questions, suggestions, or comments on the 'productivity' package, please make use of Tracker facilities at:\n",
-        "  https://r-forge.r-project.org/projects/productivity/\n"), 
-        domain = NULL, appendLF = TRUE)
+        "  https://r-forge.r-project.org/projects/productivity/\n"))
 }
